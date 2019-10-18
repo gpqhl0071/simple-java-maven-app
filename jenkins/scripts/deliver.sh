dd8aed66c293581a04a06b7dfa7fbef3311fe259
gao peng <1110@forex.com.cn>
2019-10-18 17:22:07 +0800

    add scp

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-echo 'The following Maven command installs your Maven-built Java application'
-echo 'into the local Maven repository, which will ultimately be stored in'
-echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-echo 'volume).'
 set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
@@ -24,3 +20,5 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
+
+scp target/${NAME}-${VERSION}.jar root@192.168.9.214:/gaopeng/
